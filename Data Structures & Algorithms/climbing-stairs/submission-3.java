54300c3da8cc0672bff08f6c36da05b79ee231f1
archanasuresh1710 <archana.suresh1710@gmail.com>
2026-05-16 12:09:08 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int climbStairs(int n) {
+        // n = 1, no of ways: 1 -> 1
+        // n = 2 no of ways: 1+1,2 -> 2
+        // n = 3, no: 1+1+1,1+2,2+1 -> 3
+        // n=4, no: 1+1+1+1,1+1+2,1+2+1,2+1+1,2+2 -> 5
+
+        // at step i, no of ways = no of ways for step(i-1) + no of ways for step(i-2);
+
+        if(n <=2) return n;
+
+        int prev1 = 1, prev2 = 2, step = 0;
+
+        for(int i=3;i<=n;i++) {
+            step = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = step;
+        }
+
+        return step;
+    }
+}
