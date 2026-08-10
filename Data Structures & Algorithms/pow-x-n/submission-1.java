1fc3a78e0dc8b3191d0d7aad3760a8965804c4a4
Archana Suresh <archana.suresh1710@gmail.com>
2026-08-10 17:54:00 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public double myPow(double x, int n) {
+        /*
+        approach 1: loop from 0 to n, and multiply x to itself to get x^n
+            - if given n is negative, return 1/x^n
+        time: O(n)
+
+        appraoch 2: 2^6 -> (2^3)^2 => recurse the power function with x and n/2 until it cannot be broken down further
+            -> and square the result
+            -> if n is odd, multiply x with the result
+            - give n is negative, return 1/result
+        time: each recursion call halves the n -> n/2 -> n/4 => O(logn)
+        */
+
+        double res = pow(x, Math.abs(n));
+        return n<0 ? 1/res : res;
+    }
+
+    private double pow(double x, int n) {
+
+        if(n == 0) return 1;
+
+        double half = pow(x, n/2);
+        double result = half * half;
+
+        if(n % 2 != 0) result *= x;
+
+        return result;
+    }
+}
